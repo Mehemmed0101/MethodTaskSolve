aa57a31f077075cc392af4a2271add75562a26b6
Mehemmed Qurbanov <mahammadag@code.edu.az>
2023-01-13 14:28:44 +0400

    Add project files.

---
+++ NumberTasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTaskSolve
+{
+    internal class NumberTasks
+    {
+        internal int EnterNumber()
+        {
+            Console.WriteLine("Ededi daxil edin:");
+            int n=int.Parse(Console.ReadLine());
+
+            return  n;
+        }
+        internal int GetReverseNumber(int number)
+        {
+            int reverseNumber = 0;
+            int n = (int)Math.Log10(number);
+            int r;
+
+            while(n!=0)
+            {
+                r = number % 10;
+                reverseNumber += r * (int)Math.Pow(10, n--);
+                number /= 10;
+
+            }
+
+            return reverseNumber;
+
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MethodTaskSolve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array
+            //var arrayTasks = new ArrayTasks();
+            //int[] mass = arrayTasks.EnterArray();
+            //arrayTasks.Print(mass);
+            //int[] sqrtMass = arrayTasks.GetSqrtElements(mass);
+            //arrayTasks.Print(sqrtMass);
+            #endregion
+
+            #region Number
+            //var numberTasks = new NumberTasks();
+            //int number = numberTasks.EnterNumber();
+            //int reverseNumber = numberTasks.GetReverseNumber(number);
+            //Console.WriteLine($"{number}=>{reverseNumber}");
+            #endregion
+        }
+
+    }
+}
